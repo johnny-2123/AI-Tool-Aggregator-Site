eb04d195218f2063a3e83116c32dcf98d9bbee5e
edgar <johnnyavila2021@u.northwestern.edu>
2023-10-30 20:41:02 -0500

    add logic to create new user record at /api/user POST route

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,10 +1,55 @@
 import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import { hash } from "bcrypt";
 
 export const POST = async (req) => {
   try {
     const body = await req.json();
+    console.log({ body });
     const { email, username, password } = body;
 
-    return NextResponse.json(body);
+    // check if user with email already in db
+    const existingUserByEmail = await prisma.user.findUnique({
+      where: { email: email },
+    });
+
+    if (existingUserByEmail) {
+      return NextResponse.json(
+        {
+          user: null,
+          message: "User with this email already exists",
+        },
+        { status: 409 }
+      );
+    }
+
+    // check if user with username already exists
+    const existingUserByUsername = await prisma.user.findUnique({
+      where: { username: username },
+    });
+
+    if (existingUserByUsername) {
+      return NextResponse.json(
+        {
+          user: null,
+          message: "User with this username already exists",
+        },
+        { status: 409 }
+      );
+    }
+
+    const hashedPassword = await hash(password, 10);
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return NextResponse.json({
+      user: newUser,
+      message: "Succesfully created new user",
+    });
   } catch (error) {}
 };
