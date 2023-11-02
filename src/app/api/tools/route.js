27b76e5f4c823a28d21429d9f0e1700b004ac33d
edgar <johnnyavila2021@u.northwestern.edu>
2023-11-02 12:39:01 -0500

    add database logic to submit tool api route

--- src/app/api/tools/route.js
+++ src/app/api/tools/route.js
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import prisma from "@/src/lib/prisma";
 import { hash } from "bcrypt";
 import * as z from "zod";
+import { getServerSession } from "next-auth";
+import { authConfig } from "@/src/lib/auth";
 
 const toolSchema = z.object({
   url: z.string().min(1, "URL is required.").url("Must be a valid URL."),
@@ -13,15 +15,61 @@ const toolSchema = z.object({
   category: z
     .array(z.string())
     .refine((value) => value.some((category) => category), {
-      message: "You have to select at least one categories.",
+      message: "You must select at least one category.",
     }),
+  imageUrl: z.string().min(1, { message: "image is required" }),
 });
 
-export const POST = async (req) => {
+export const POST = async (req, res) => {
+  const session = await getServerSession(authConfig);
+  if (!session) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+  console.log("session in tools api route", session);
   try {
     const body = await req.json();
     console.log("body in server: ", body);
-    const { url, title, description, pricing, imageUrl, categoryIds } = body;
+    const {
+      url,
+      title,
+      description,
+      pricing,
+      imageUrl,
+      category: categoryNames,
+    } = toolSchema.parse(body);
+
+    const categories = await prisma.category.findMany({
+      where: {
+        name: {
+          in: categoryNames,
+        },
+      },
+    });
+    console.log("categories", categories);
+
+    const newApp = await prisma.app.create({
+      data: {
+        url,
+        title,
+        description,
+        pricing,
+        imageUrl,
+        userId: session.user.id,
+        categories: {
+          create: categories.map((category) => ({
+            category: {
+              connect: { id: category.id },
+            },
+          })),
+        },
+      },
+      include: {
+        categories: true,
+      },
+    });
+
+    console.log("new app", newApp);
+
     return NextResponse.json({
       message: "hello from tool route",
     });
