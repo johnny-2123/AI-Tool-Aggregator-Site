43bee5beda6e988aac5877c45f8b0d81d1e41cc1
edgar <johnnyavila2021@u.northwestern.edu>
2023-11-02 02:26:17 -0500

    handle form submit

--- src/components/form/SubmitToolForm.jsx
+++ src/components/form/SubmitToolForm.jsx
@@ -5,40 +5,44 @@ import { useState } from "react";
 import { useEdgeStore } from "@/src/lib/edgestore";
 import { Progress } from "@/src/components/ui/progress";
 import { Input } from "@/src/components/ui/input";
+import { Form } from "@/src/components/ui/form";
+import { Button } from "@/src/components/ui/button";
 
 export default function Page() {
   const [file, setFile] = React.useState(null);
   const [progress, setProgress] = useState(0);
   const { edgestore } = useEdgeStore();
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (file) {
+      const res = await edgestore.myPublicImages.upload({
+        file,
+        onProgressChange: (progress) => {
+          setProgress(progress);
+          console.log(progress);
+        },
+      });
+      // run some server action or api here
+      // to add the necessary data to your database
+      console.log(res);
+    }
+  };
+
   return (
-    <div className="flex flex-col items-center w-3/5 mx-auto">
-      <Input
-        type="file"
-        onChange={(e) => {
-          setFile(e.target.files?.[0]);
-        }}
-      />
-      <button
-        onClick={async () => {
-          if (file) {
-            const res = await edgestore.myPublicImages.upload({
-              file,
-              onProgressChange: (progress) => {
-                // you can use this to show a progress bar
-                setProgress(progress);
-                console.log(progress);
-              },
-            });
-            // you can run some server action or api here
-            // to add the necessary data to your database
-            console.log(res);
-          }
-        }}
-      >
-        Upload
-      </button>
-      <Progress value={progress} />
-    </div>
+    <Form
+    // className="flex flex-col items-center w-3/5 mx-auto"
+    >
+      <form onSubmit={handleSubmit}>
+        <Input
+          type="file"
+          onChange={(e) => {
+            setFile(e.target.files?.[0]);
+          }}
+        />
+        <Button type="submit">Upload</Button>
+        <Progress value={progress} />
+      </form>
+    </Form>
   );
 }
